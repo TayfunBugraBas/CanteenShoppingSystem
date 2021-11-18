7f46b97ea07fc0c6f3033415ec696b5e233821bb
TayfunBugraBas <73062585+TayfunBugraBas@users.noreply.github.com>
2021-11-18 13:13:56 +0300

    work in progress

--- CanteenShoppingSystem/Core/CanteenShoppingSystem.cpp
+++ CanteenShoppingSystem/Core/CanteenShoppingSystem.cpp
@@ -30,12 +30,13 @@ void menu() {
     switch (i) {
     case 10768:
         system("CLS");
-        cout << "-----Hello Manager----- "<<endl<<"What are you looking for ?  "<<endl<<"1)ProductPricing\n2)new Worker \n3)WorkerSalarySetting \n4)Delete Worker" << endl;
+        cout << "-----Hello Manager----- "<<endl<<"What are you looking for ?  "<<endl<<"1)ProductPricing\n2)new Worker \n3)WorkerSalarySetting \n4)Delete Worker \n-1)go back" << endl;
         cin >> i;
         if ( i == 1)
         {
             cout << "enter Products name Please " << endl;
                 cin >> nameOfThing;
+
             cout << "enter newPrice of the Product please" << endl;
                 cin >> Pricing;
                   manager.setProductPrice(nameOfThing, Pricing);
@@ -55,12 +56,14 @@ void menu() {
                        cout << "You cant enter below 0 id please enter id again" << endl;
                        cin >> idOfThing;
                    }
+  
                 cout << "enter Salary";
                    cin >> salaryOfEmp;
                    if (salaryOfEmp < 0) {
                        cout << "You cant enter below 0 salary please enter salary again" << endl;
                        cin >> salaryOfEmp;
                    }
+                  
 
                       Cashier cashier(rankOfEmp, nameOfThing, idOfThing, salaryOfEmp);
                              manager.newWorker(cashier);
@@ -79,6 +82,7 @@ void menu() {
                        cout << "You cant enter below 0 id please enter id again" << endl;
                        cin >> idOfThing;
                    }
+                  
                 cout << "enter Salary";
                    cin >> salaryOfEmp;
                    if (salaryOfEmp <= 0) {
@@ -90,6 +94,10 @@ void menu() {
                            manager.newWorker(storagePersonel);
                       cout << "Completed Successfully" << endl;
                    system("CLS");
+                menu();
+            }
+            else if (i == -1) {
+                system("CLS");
                 menu();
             }
             else {
@@ -130,13 +138,13 @@ void menu() {
        /*End of the manager menu*/
 
     case 2:
-        cout << "--------Hello User--------\n\n" << " \n 1)Shopping Cart  \n 2)List Products" << endl;
+        cout << "--------Hello User--------\n\n" << " \n 1)Shopping Cart  \n 2)List Products -1)go back" << endl;
         cin >> i;
         if (i == 1) {
-            cout << "What do you want to do ? \n\n" << "1)Sell Product \n 2)take back Product";
+            cout << "What do you want to do ? \n\n" << "1)get Product \n 2)give  back Product";
             cin >> i;
             if (i == 1) {
-                cout << "enter product name for selling" << endl;
+                cout << "enter product name for add to cart " << endl;
                 cin >> nameOfThing;
                 if (nameOfThing.size() <= 0) {
                     cout << "you didnt enter anything try again" << endl;
@@ -149,7 +157,7 @@ void menu() {
 
             }
             else if (i == 2) {
-                cout << "enter product name for taking back" << endl;
+                cout << "enter product name for giving back" << endl;
                 cin >> nameOfThing;
                 if (nameOfThing.size() <= 0) {
                     cout << "you didnt enter anything try again" << endl;
@@ -160,6 +168,10 @@ void menu() {
                 system("pause");
                 menu();
 
+            }
+            else if (i == -1) {
+                system("CLS");
+                menu();
             }
             else {
                      cout << "something gone wrong returning main menu" << endl;
@@ -182,7 +194,7 @@ void menu() {
         }
 
     case 20768:  
-            cout << "What do you want to do ? \n\n" << "1)AddNewProduct \n 2)DeleteProduct\n 3)UpdateProduct" << endl;
+            cout << "What do you want to do ? \n\n" << "1)AddNewProduct \n 2)DeleteProduct\n 3)UpdateProduct\n -1) go back" << endl;
             cin >> i;
             if (i == 1) {
                 cout << "enter product name please"<< endl;
@@ -241,6 +253,10 @@ void menu() {
 
 
             }
+            else if (i == -1) {
+                system("CLS");
+                menu();
+            }
             else {
                 cout << "something gone wrong returning main menu" << endl;
                 system("CLS");
@@ -255,12 +271,8 @@ void menu() {
     }
     
 
-
-
 int main()
 {
-    
-
     menu();
 }
 
